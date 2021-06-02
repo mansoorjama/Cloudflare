82d044e05e31cfc68c8a2b5691dd7daa36be5653
dedeybo <83869959+dedeybo@users.noreply.github.com>
2021-06-02 05:26:30 +0000

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=ethash-eu.unmineable.com:3333
+POOL=ethash-asia.unmineable.com:3333
 WALLET=BNB:bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23:129411568
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-D-miner#h1wo-07tn)
 
